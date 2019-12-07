39431b13c9670e46bc9f3f6c888e35d8c4d445af
gazorpazorp85 <58003414+gazorpazorp85@users.noreply.github.com>
2019-12-07 17:28:19 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+'use strict'
+
+function toggleMenu() {
+    document.body.classList.toggle('menu-open');
+    let elButton = document.querySelectorAll('.menu-btn');
+    for (let i = 0; i < elButton.length; i++) {
+        elButton[i].classList.toggle('hidden');
+    }
+}
